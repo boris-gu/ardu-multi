acc112d6e283e587798a0bdd8a3804901f429962
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2025-12-01 00:44:18 +0300

    Update colors of log

--- ardu-multi.py
+++ ardu-multi.py
@@ -10,6 +10,8 @@ from textual.widgets import Header, Footer, DataTable, TabbedContent, TabPane, R
 from textual.worker import get_current_worker
 from rich.text import Text
 
+DEBUG = False
+
 apm_copter_mode = ('Stabilize',    'Acro',            'AltHold',  'Auto',   'Guided',
                    'Loiter',       'RTL',             'Circle',   '???',    'Land',
                    '???',          'Drift',           '???',      'Sport',  'Flip',
@@ -17,14 +19,15 @@ apm_copter_mode = ('Stabilize',    'Acro',            'AltHold',  'Auto',   'Gui
                    'Guided_NoGPS', 'Smart_RTL',       'FlowHold', 'Follow', 'ZigZag',
                    'SystemID',     'Heli_Autorotate', 'Auto RTL', 'Turtle')
 
-text_severity = (('EMERGENCY', 'red'),
-                 ('ALERT',     'red'),
-                 ('CRITICAL',  'red'),
-                 ('ERROR',     'red'),
-                 ('WARNING',   'dark_orange3'),
-                 ('NOTICE',    'dark_orange3'),
-                 ('INFO',      ''),
-                 ('DEBUG',     ''))
+all_sev = (('EMERGENCY', 'white on #8D0638'),
+           ('ALERT',     'white on #8D0638'),
+           ('CRITICAL',  'white on #8D0638'),
+           ('ERROR',     'white on #8D0638'),
+           ('WARNING',   'white on #B36800'),
+           ('NOTICE',    'white on #B36800'),
+           ('INFO',      'reverse'),
+           ('DEBUG',     'reverse'),
+           ('APP DEBUG',      'reverse'))
 
 # {com_ports:[drone_id]}
 telems: dict[mavutil.mavserial, list[int]] = {}
@@ -73,7 +76,9 @@ class ArduMultiApp(App):
         worker = get_current_worker()
         table = self.query_one('#table_log', DataTable)
         # Обновление данных
-        i = 0
+        if DEBUG:
+            for i in range(len(all_sev)):
+                self.call_from_thread(self.print_textlog, 'Lorem ipsum dolor', i)
         while True:
             if worker.is_cancelled:
                 return
@@ -82,13 +87,9 @@ class ArduMultiApp(App):
                     try: # Ловим, если порт был отключен
                         rx_msg = telem.recv_match(blocking=False)
                     except (serial.SerialException, serial.SerialTimeoutException):
-                        self.call_from_thread(self.print_textlog, None,
-                                              f'{text_severity[1][0]}: {telem.port.name} Disconnected',
-                                              text_severity[1][1])
+                        self.call_from_thread(self.print_textlog, f'{telem.port.name} Disconnected', 2)
                         for id in telems[telem]:
-                            self.call_from_thread(self.print_textlog, id,
-                                                  f'{text_severity[1][0]}: Disconnected',
-                                                  text_severity[1][1])
+                            self.call_from_thread(self.print_textlog, f'Disconnected', 2, id)
                         del telems[telem]
                         break
                     if rx_msg is None:
@@ -134,23 +135,11 @@ class ArduMultiApp(App):
                         elif rx_msg.get_msgId() == mav2.MAVLINK_MSG_ID_STATUSTEXT:
                             rx_text: mav2.MAVLink_statustext_message = rx_msg
                             # TODO: Как-то проверить текст из нескольких чанков
-                            self.call_from_thread(self.print_textlog, rx_sysid,
-                                                  f'{text_severity[rx_text.severity][0]}: {rx_text.text}',
-                                                  text_severity[rx_text.severity][1])
-                        else: # Неизвестное сообщение
-                            # XXX: Раскомментировать для дебага
-                            # self.call_from_thread(self.print_textlog, rx_sysid, f'Message {rx_msg.get_type()} received')
-                            pass
-                    # TODO: Удалить после проверки
-                    # self.call_from_thread(self.print_textlog, 2,
-                    #                       f'{text_severity[i % len(text_severity)][0]}: Test',
-                    #                       text_severity[i % len(text_severity)][1])
-                    # i += 1
-                    # time.sleep(1)
+                            self.call_from_thread(self.print_textlog, rx_text.text, rx_text.severity, rx_sysid)
+                        elif DEBUG:
+                            self.call_from_thread(self.print_textlog, f'Message {rx_msg.get_type()} received', -1, rx_sysid)
             except Exception as e:
-                self.call_from_thread(self.print_textlog, None,
-                                      f'{text_severity[0][0]}: App Failure ({e})',
-                                      text_severity[0][1])
+                self.call_from_thread(self.print_textlog, f'App Failure ({e})', 0)
                 return
 
     def new_drone(self, telem, id):
@@ -165,14 +154,14 @@ class ArduMultiApp(App):
         # Добавление ID в словарь
         telems[telem].append(id)
 
-    def print_textlog(self, id, text: str, style: str=''):
+    def print_textlog(self, text: str, severity=-1, id=None):
         prefix = time.strftime('[%H:%M:%S]', time.localtime())
         if id:
             textlog = self.query_one(f'#textlog_{id}', RichLog)
-            textlog.write(Text(f'{prefix} {text}', style=style))
+            textlog.write(Text.from_markup(f'{prefix} [{all_sev[severity][1]}] {all_sev[severity][0]} [/] {text}'))
             prefix += f' [Drone {id}]'
         textlog = self.query_one('#textlog_all', RichLog)
-        textlog.write(Text(f'{prefix} {text}', style=style))
+        textlog.write(Text.from_markup(f'{prefix} [{all_sev[severity][1]}] {all_sev[severity][0]} [/] {text}'))
 
 def com_parse(arg):
     try:
@@ -180,6 +169,7 @@ def com_parse(arg):
         return (port.upper(), int(baudrate))
     except:
         raise argparse.ArgumentTypeError(f'[{arg}] use format PORT:BAUDRATE')
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
